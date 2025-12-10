a4429877e2ebd15210c5282d201be8f073658254
cosoto1 <Cosoto1@cps.edu>
2025-12-10 19:19:53 +0000

    While Loops

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -4,3 +4,11 @@ colors = ["red", "blue", "green", "yellow", "purple"]
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+
+index = 0
+while index < len(colors):
+    if colors[index] == "yellow":
+        break
+    print(colors[index])
+    index += 1
+
